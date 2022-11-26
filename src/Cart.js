a92ca248a26a394c3a0774d179cd5d1bb3c2192d
Deepak Kumar <kumar357deepak@gmail.com>
2022-11-26 13:19:58 +0530

    adding props in cartitem

--- src/Cart.js
+++ src/Cart.js
@@ -2,10 +2,72 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            products: [
+                {
+                    title: 'Mobile',
+                    price: 9999,
+                    qty: 1,
+                    id: 1,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpwhD6sYLoaWpUxIPK0Ra631k0pVDwJhC_Pw&usqp=CAU'
+                },
+                {
+                    title: 'Watch',
+                    price: 999,
+                    qty: 1,
+                    id: 2,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpwhD6sYLoaWpUxIPK0Ra631k0pVDwJhC_Pw&usqp=CAU'
+                },
+                {
+                    title: 'Laptop',
+                    price: 45000,
+                    qty: 1,
+                    id: 3,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpwhD6sYLoaWpUxIPK0Ra631k0pVDwJhC_Pw&usqp=CAU'
+                }
+            ]
+        }
+    }
+
+    increasedQuantity(){
+        // form 1
+        // this.setState({
+        //     qty: this.state.qty +1
+        // })
+
+        //form 2nd
+        this.setState((prevState)=>{
+            return {
+                qty: prevState.qty+1
+            }
+        })
+    }
+
+    decresedQuantity = ()=>{
+        if(this.state.qty ===0){
+            return;
+        }
+
+        this.setState({
+            qty: this.state.qty -1
+        })
+
+    }
+
     render(){
+        const {products} = this.state;
         return(
             <div>
-                <CartItem />
+                {products.map((product)=>{
+                    return <CartItem 
+                    product= {product}
+                    key ={product.id}
+                     />
+                })}
             </div>
         );
     }    
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,50 +2,50 @@ import React from 'react';
 
 class CartItem extends React.Component{
 
-    constructor(){
-        super();
+    // constructor(){
+    //     super();
 
-        this.state = {
-            title: 'Mobile',
-            price: 999,
-            qty: 1,
-            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpwhD6sYLoaWpUxIPK0Ra631k0pVDwJhC_Pw&usqp=CAU'
-        }
-    }
+    //     this.state = {
+    //         title: 'Mobile',
+    //         price: 999,
+    //         qty: 1,
+    //         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpwhD6sYLoaWpUxIPK0Ra631k0pVDwJhC_Pw&usqp=CAU'
+    //     }
+    // }
 
-    increasedQuantity(){
-        // form 1
-        // this.setState({
-        //     qty: this.state.qty +1
-        // })
+    // increasedQuantity(){
+    //     // form 1
+    //     // this.setState({
+    //     //     qty: this.state.qty +1
+    //     // })
 
-        //form 2nd
-        this.setState((prevState)=>{
-            return {
-                qty: prevState.qty+1
-            }
-        })
-    }
+    //     //form 2nd
+    //     this.setState((prevState)=>{
+    //         return {
+    //             qty: prevState.qty+1
+    //         }
+    //     })
+    // }
 
-    decresedQuantity = ()=>{
-        if(this.state.qty ===0){
-            return;
-        }
+    // decresedQuantity = ()=>{
+    //     if(this.state.qty ===0){
+    //         return;
+    //     }
 
-        this.setState({
-            qty: this.state.qty -1
-        })
+    //     this.setState({
+    //         qty: this.state.qty -1
+    //     })
 
-    }
+    // }
 
     render(){
-
-        const {title,  qty, price} = this.state;
+        console.log(this.props)
+        const {title,  qty, price, img} = this.props.product;
 
         return(
             <div className='cart-item'>
             <div className='left-block'>
-                <img  style={styles.images } alt="" src= {this.state.img} />
+                <img  style={styles.images } alt="" src= {img} />
             </div>
 
             <div className='right-block'>
@@ -58,13 +58,13 @@ class CartItem extends React.Component{
                      src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
                      alt="increase"
                      className='action-icons'
-                     onClick={this.increasedQuantity.bind(this)}
+                     
                     />
                     <img
                       src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
                       alt="decrese" 
                       className='action-icons'
-                      onClick={this.decresedQuantity}
+                      
                     />
                     <img
                       src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg"
